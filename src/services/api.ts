f0442415f92b1a2b85230bbef575e210c6e9fb66
Matheus Moreira <matheusmoreira08@gmail.com>
2022-11-18 22:40:47 -0300

    att end server api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
     // baseURL: 'http://192.168.0.22:3333/'
-    baseURL: 'http://192.168.0.207:3333/'
+    baseURL: process.env.BASE_URL
 })
